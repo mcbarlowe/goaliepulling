ed86148bf86cafb3e3e3d651efaf3bf928fe2850
mcbarlowe <mcbarlowe@gmail.com>
2020-11-16 22:56:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Goalie Pulling Analysis Code
+
+This is a couple of recursive functions to calculate the Expected Points values for a team given score differential,
+time left, and whether they pull their goalie or not. The original concept can be found in the
+[Pulling  the Goalie: Hockey and Investment Implications](http://static.ow.ly/docs/Pulling%20the%20Goalie%20Hockey%20and%20Investment%20Implications_7nSt.pdf)
+paper by Clifford Asness and Aaron Brown. As of right now it funs fast for up to ~5 minutes left in the game. I haven't
+tested further but I'm sure there are optimizations to be made in the code. I you have have any please submit a pull request.
+Thanks hope people find this helpful.
---
+++ yoloscript.py
@@ -0,0 +1,103 @@
+import math
+from functools import lru_cache
+
+
+@lru_cache
+def ep_np(score_diff, time):
+    """
+    Function calculates the expected point values of a team who doesn't pull
+    their goalie at all no matter what
+
+    Inputs:
+    score_diff - The score differential as a positive or negative integer
+    time  - seconds left in the game
+
+    Outputs:
+    expected_points  - the expected point value of a team given the score_diff difference
+                       and time left in the game
+    """
+    if time == 0:
+        if score_diff < 0:
+            if score_diff < -1:
+                return 0
+            elif score_diff == -1:
+                return 0.0065 * 1.5
+        elif score_diff == 0:
+            return (0.987 * 1.5) + (0.0065 * 2)
+        else:
+            if score_diff > 1:
+                return (0.987 * 2) + (0.0065 * 2) + (0.0065 * 2)
+            elif score_diff == 1:
+                return (0.987 * 2) + (0.0065 * 2) + (0.0065 * 1.5)
+    else:
+        time_segments = math.floor(time / 10)
+        prob_score_diff_is_same = 1 - ((0.0065 * (time_segments)) * 2)
+
+        return (
+            (prob_score_diff_is_same * ep_np(score_diff, time - 10))
+            + ((time_segments * 0.0065) * ep_np(score_diff + 1, time - 10))
+            + ((time_segments * 0.0065) * ep_np(score_diff - 1, time - 10))
+        )
+
+
+@lru_cache
+def ep_po(score_diff, time):
+    """
+    Function calculates the expected point values of a team who could or could
+    not pull their goalie depending on the situation
+
+    Inputs:
+    score_diff - The score differential as a positive or negative integer
+    time  - seconds left in the game
+
+    Outputs:
+    expected_points  - the expected point value of the team
+    """
+    if time == 0:
+        if score_diff < 0:
+            if score_diff < -1:
+                return 0
+            elif score_diff == -1:
+                return 0.0118 * 1.5
+        elif score_diff == 0:
+            return (0.9624 * 1.5) + (0.0118 * 2)
+        else:
+            if score_diff > 1:
+                return (0.9624 * 2) + (0.0118 * 2) + (0.0118 * 2)
+            elif score_diff == 1:
+                return (0.9624 * 2) + (0.0118 * 2) + (0.0258 * 1.5)
+    else:
+        time_segments = math.floor(time / 10)
+        prob_score_diff_is_same = (
+            1 - (0.0118 * (time_segments)) - (0.0258 * (time_segments))
+        )
+
+        return (
+            (
+                prob_score_diff_is_same
+                * max(
+                    ep_po(score_diff, time - 10),
+                    ep_np(score_diff, time - 10),
+                )
+            )
+            + (
+                (time_segments * 0.0118)
+                * max(
+                    ep_po(score_diff + 1, time - 10),
+                    ep_np(score_diff + 1, time - 10),
+                )
+            )
+            + (
+                (time_segments * 0.0258)
+                * max(
+                    ep_po(score_diff - 1, time - 10),
+                    ep_np(score_diff - 1, time - 10),
+                )
+            )
+        )
+
+
+if __name__ == "__main__":
+    print(ep_np(-1, 10))
+    print(ep_np(-2, 200))
+    print(ep_po(-2, 300))
