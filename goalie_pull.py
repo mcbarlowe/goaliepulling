d8146d816432ead668b7197b8c23dd90ec5d2348
mcbarlowe <mcbarlowe@gmail.com>
2020-11-17 10:26:22 -0500

    Added keywoard args so one can adjust scoring probabilites

--- goalie_pull.py
+++ goalie_pull.py
@@ -3,7 +3,13 @@ from functools import lru_cache
 
 
 @lru_cache
-def ep_np(score_diff, time):
+def ep_np(
+    score_diff: int,
+    time: int,
+    prob_of_no_score: float=0.987,
+    prob_team_scores: float=0.0065,
+    prob_team_concedes: float=0.0065,
+) -> float:
     """
     Function calculates the expected point values of a team who doesn't pull
     their goalie at all no matter what
@@ -11,6 +17,9 @@ def ep_np(score_diff, time):
     Inputs:
     score_diff - The score differential as a positive or negative integer
     time  - seconds left in the game
+    prob_of_no_score - probability of a neither team scoring in a ten second block
+    prob_team_scores - probability of a team scoring in a ten second block
+    prob_team_concedes - probability of a team conceding a goal in a ten second block
 
     Outputs:
     expected_points  - the expected point value of a team given the score_diff difference
@@ -21,25 +30,31 @@ def ep_np(score_diff, time):
             if score_diff < -1:
                 return 0
             elif score_diff == -1:
-                return 0.0065 * 1.5
+                return prob_team_scores * 1.5
         elif score_diff == 0:
-            return (0.987 * 1.5) + (0.0065 * 2)
+            return (prob_of_no_score * 1.5) + (prob_team_scores * 2)
         else:
             if score_diff > 1:
-                return (0.987 * 2) + (0.0065 * 2) + (0.0065 * 2)
+                return (prob_of_no_score * 2) + (prob_team_scores * 2) + (prob_team_concedes * 2)
             elif score_diff == 1:
-                return (0.987 * 2) + (0.0065 * 2) + (0.0065 * 1.5)
+                return (prob_of_no_score * 2) + (prob_team_scores * 2) + (prob_team_concedes * 1.5)
     else:
 
         return (
-            (0.987 * ep_np(score_diff, time - 10))
-            + (0.0065 * ep_np(score_diff + 1, time - 10))
-            + (0.0065 * ep_np(score_diff - 1, time - 10))
+            (prob_of_no_score * ep_np(score_diff, time - 10))
+            + (prob_team_scores * ep_np(score_diff + 1, time - 10))
+            + (prob_team_concedes * ep_np(score_diff - 1, time - 10))
         )
 
 
 @lru_cache
-def ep_po(score_diff, time):
+def ep_po(
+        score_diff: int,
+        time: int,
+        prob_of_no_score: float=0.9624,
+        prob_team_scores: float=0.0118,
+        prob_team_concedes: float=0.0258
+        ) -> float:
     """
     Function calculates the expected point values of a team who could or could
     not pull their goalie depending on the situation
@@ -47,6 +62,9 @@ def ep_po(score_diff, time):
     Inputs:
     score_diff - The score differential as a positive or negative integer
     time  - seconds left in the game
+    prob_of_no_score - probability of a neither team scoring in a ten second block
+    prob_team_scores - probability of a team scoring in a ten second block
+    prob_team_concedes - probability of a team conceding a goal in a ten second block
 
     Outputs:
     expected_points  - the expected point value of the team
@@ -56,32 +74,32 @@ def ep_po(score_diff, time):
             if score_diff < -1:
                 return 0
             elif score_diff == -1:
-                return 0.0118 * 1.5
+                return prob_team_scores * 1.5
         elif score_diff == 0:
-            return (0.9624 * 1.5) + (0.0118 * 2)
+            return (prob_of_no_score * 1.5) + (prob_team_scores * 2)
         else:
             if score_diff > 1:
-                return (0.9624 * 2) + (0.0118 * 2) + (0.0118 * 2)
+                return (prob_of_no_score * 2) + (prob_team_scores * 2) + (prob_team_concedes * 2)
             elif score_diff == 1:
-                return (0.9624 * 2) + (0.0118 * 2) + (0.0258 * 1.5)
+                return (prob_of_no_score * 2) + (prob_team_scores * 2) + (prob_team_concedes * 1.5)
     else:
         return (
             (
-                0.9624
+                prob_of_no_score
                 * max(
                     ep_po(score_diff, time - 10),
                     ep_np(score_diff, time - 10),
                 )
             )
             + (
-                0.0118
+                prob_team_scores
                 * max(
                     ep_po(score_diff + 1, time - 10),
                     ep_np(score_diff + 1, time - 10),
                 )
             )
             + (
-                0.0258
+                prob_team_concedes
                 * max(
                     ep_po(score_diff - 1, time - 10),
                     ep_np(score_diff - 1, time - 10),
